4956509e1659baf96a4a1ca8a125e197a70b9a35
Tomasz Świętek <tswietek@DESKTOP-KR5ETAP.localdomain>
2019-04-17 17:33:16 +0200

    primitive navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <a href="/leaderboard">Leaderboard </a>
+        <a href="/home">Home</a>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
