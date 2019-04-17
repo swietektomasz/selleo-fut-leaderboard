4b51c95a0a69459f13e753760e20c979760793ac
Tomasz Świętek <tswietek@DESKTOP-KR5ETAP.localdomain>
2019-04-17 17:33:33 +0200

    simple list to see if it works

---
+++ src/containers/TournamentList/TournamentList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TournamentList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>first tournament</li>
+        <li>second tournament</li>
+        <li>third tournament</li>
+        <li>fourth tournament</li>
+        <li>last tournament</li>
+      </ul>
+    );
+  }
+}
+
+export default TournamentList;
